48d214a00cfc6e28aba4bbf9e7396c8a3ceef97e
AlBuynovskiy <AlBuynovskiy@gmail.com>
2022-12-14 19:22:14 +0300

    Task condition created

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать string[].
+*/
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
