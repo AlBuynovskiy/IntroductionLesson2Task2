ca7644709544aacac797246988c58dee3ab1346c
Aliaksandr Buinouski <AlBuynovskiy@gmail.com>
2022-12-15 14:59:59 +0300

    Working version of the program

--- Program.cs
+++ Program.cs
@@ -6,4 +6,35 @@
 Console.WriteLine("Введите число: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
+int a = 99;
+int b = 1000;
+int c = 10;
+int result1;
 
+if (number < a)
+{
+    Console.WriteLine($"Вы ввели число {number}");
+    Console.WriteLine($"Число {number} не является трехзначным");
+}
+else
+
+if (number < b)
+{
+    Console.WriteLine($"Вы ввели число {number}, являющееся трехзначным числом");
+    result1 = number % c;
+    Console.WriteLine($"Третья цифра числа {number} равна {result1}");
+}
+else
+
+if (number > b)
+{
+    Console.WriteLine($"Вы ввели число {number}");
+    Console.Write($"Число {number} преобразовано в трехзначаное число ");
+    for(; number > b; number /= c)
+    {
+        result1 = number;
+    }
+    Console.WriteLine(number);
+    int result2 = number % c;
+    Console.WriteLine($"Третье число числа {number} равно {result2}");
+}
